00f0e95d852e3e60c611cfe5778ba7dd68499bf5
Matheus Henrique Bonfim da Silva <104633582+RexDetona@users.noreply.github.com>
2023-03-22 08:56:00 -0300

    Add project files.

---
+++ MatheusinAppWeb/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using MatheusinAppWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace MatheusinAppWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Bemvindo()
+        {
+            return View();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MatheusinAppWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MatheusinAppWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
